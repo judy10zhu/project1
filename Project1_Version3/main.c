c451ec91c157756a625839456ced871d1cf3ffbd
JENNIESHU <90105666+JENNIESHU@users.noreply.github.com>
2021-09-12 23:11:45 -0400

    update main.c
    add the second DFA and user iput

--- Project1_Version3/main.c
+++ Project1_Version3/main.c
@@ -20,74 +20,29 @@ int main() {
 	DFA_set_transition(d, 6, 'e', 7);
 	DFA_set_transition(d, 7, 'r', 8);
 	DFA_set_acceptingState(d, 8);
-	//DFA_print(d);
 
-	bool result = DFA_execute(d, "computer");
+	//ask user for input
+    char userInput[100];
+    printf("Enter your input: ");
+    scanf("%s", &userInput);
+	bool result = DFA_execute(d, userInput);
 	printf("%d", result);
-	//printf("%d", DFA_get_accepting(d, 4));
-	//print_state(get_nextState(DFA_get_initialState(d)));
-	//print_state(DFA_find_state(d, 1));
 
+	//DFA for Any string that starts with 123
+	DFA d = new_dfa(4);
+	DFA_set_transition(d, 0, '1', 1);
+	DFA_set_transition(d, 1, '2', 2);
+	DFA_set_transition(d, 2, '3', 3);
+	DFA_set_acceptingState(d, 3);
 
-//
-	//printf("%d", DFA_get_size(d));
-//	State s1 = new_state(0);
-//	add_transition(s1, 'c', 1);
-//	print_state(s1);
-//
-//	State s2 = new_state(1);
-//	add_transition(s2, 'o', 2);
-//	set_nextState(s1, s2);
-//
-//	State s3 = new_state(2);
-//	add_transition(s3, 'm', 3);
-//	set_nextState(s2, s3);
-//
-//	State s4 = new_state(3);
-//	add_transition(s4, 'p', 4);
-//	set_nextState(s3, s4);
-//
-//	State s5 = new_state(4);
-//	add_transition(s5, 'u', 5);
-//	set_nextState(s4, s5);
-//
-//	State s6 = new_state(5);
-//	add_transition(s6, 't', 6);
-//	set_nextState(s5, s6);
-//
-//	State s7 = new_state(6);
-//	add_transition(s7, 'e', 7);
-//	set_nextState(s6, s7);
-//
-//	State s8 = new_state(7);
-//	set_accept(s8, true);
-//	add_transition(s8, 'r', 8);
-//	set_nextState(s7, s8);
-//
-//	State s = get_nextState(s1);
-//	while(s != NULL){
-//		print_state(s);
-//		s = get_nextState(s);
-//	}
+	//ask user for input
+	char userInput[100];
+	printf("Enter your input: ");
+	scanf("%s", &userInput);
+	bool result = DFA_execute(d, userInput);
+	printf("%d", result);
 
+	//DFA for Binary input with an even number of 0’s and an even number of 1’s
 
-//	printf("%d", search_char(s1, 'o'));
-//	free_state(s1);
-
-	//    DFA d = new_DFA(9);g
-	//
-	//    printf("%d \n",DFA_get_size(d));
-	//    DFA_set_transition(d, 0, 'c', 1);
-	//    DFA_set_transition(d, 1, 'o', 2);
-	//    DFA_set_transition(d, 2, 'm', 3);
-	//    DFA_set_transition(d, 3, 'p', 4);
-	//    DFA_set_transition(d, 4, 'u', 5);
-	//    DFA_set_transition(d, 5, 't', 6);
-	//    DFA_set_transition(d, 6, 'e', 7);
-	//    DFA_set_transition(d, 7, 'r', 8);
-	//
-	//    printf("done \n");
-	//
-	//    DFA_print(d);
 
 }
